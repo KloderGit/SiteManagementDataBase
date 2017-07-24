56ff74cbf35965b56bfc95a5589c6f83e20ae98d
Kloder <kloder3@gmail.com>
2017-07-24 16:39:57 +0300

    Init DataBase Entity Project

---
+++ DataBase.EntityFramework/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataBase.EntityFramework
+{
+    public class Class1
+    {
+    }
+}
---
+++ DataBase.Interfaces/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataBase.Interfaces
+{
+    public class Class1
+    {
+    }
+}
